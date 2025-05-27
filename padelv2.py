cf7993a6da43cdfd32f8e725b93dd80f11c604ae
B3Dchris <123953949+B3Dchris@users.noreply.github.com>
2025-05-27 09:20:38 +0200

    Update padelv2.py

--- padelv2.py
+++ padelv2.py
@@ -12,6 +12,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
 
 # Setup
 load_dotenv()
